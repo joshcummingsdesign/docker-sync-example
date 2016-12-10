a2bc24c88b987bfdc8765b219c3ae48d2c87bc31
josh <josh@madebygrizzly.com>
2016-12-10 13:04:49 -0800

    Adds live reloading via gulp

--- README.md
+++ README.md
@@ -8,10 +8,7 @@ An example of docker-sync in action!
 
 ## Getting Started
 
+1. Run `npm install` to install dependencies
 1. Run `gulp` to compile assets
-2. Run `docker-sync-stack start` to fire up docker-sync
-3. In another terminal window run `gulp watch` to watch for sass changes
-
-## Known issues
-
-* You have to save a couple of times because browser-sync can inject css into the page faster than docker-sync can rsync
+2. Run `docker-sync-stack start` to start containers
+3. In another terminal window run `gulp watch` to watch for changes
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,37 @@
 var browserSync = require('browser-sync').create(),
+    exec        = require('child_process').exec,
     gulp        = require('gulp'),
+    runSequence = require('gulp-run-sequence'),
     sass        = require('gulp-sass');
+
+gulp.task('dockerSync', function (cb) {
+  exec('docker-sync sync', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('reload', function (done) {
+  browserSync.reload();
+  done();
+});
 
 gulp.task('sass', function () {
   return gulp.src('./src/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./app/dist'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('./app/dist'));
+});
+
+gulp.task('sass:watch', function () {
+  runSequence('sass', 'dockerSync', 'reload');
 });
 
 gulp.task('watch', function () {
   browserSync.init({
     proxy: 'http://localhost:8080'
   });
-  gulp.watch('./src/**/*.scss', ['sass']);
+  gulp.watch('./src/**/*.scss', ['sass:watch']);
 });
 
 gulp.task('default', ['sass']);
